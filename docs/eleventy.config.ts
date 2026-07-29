b0669750da76a2341360e812b3860aa80b102dab
vxppy <earthingout01@gmail.com>
2026-07-29 20:13:16 +0530

    fixed prefix path again

--- docs/eleventy.config.ts
+++ docs/eleventy.config.ts
@@ -41,7 +41,7 @@ export default defineConfig(async (config) => {
     config.addFilter('markdown', (value: string) => markdownIt.render(value));
 
     return {
-        pathPrefix: 'https://vxppy.github.io/discord-components',
+        pathPrefix: '/discord-components/',
         dir: {
             input: 'src',
             output: '_site',
